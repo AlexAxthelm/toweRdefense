f5efa7dea90c658b5e9e4ed31ada7c0239795975
Alex Axthelm <aaxthelm@RMI.org>
2024-08-07 22:56:02 +0200

    Add infrastructure for map

---
+++ R/map.R
@@ -0,0 +1,45 @@
+map <- S7::new_class("map",
+  properties = list(
+    width = S7::class_integer,
+    height = S7::class_integer,
+    cells = S7::class_data.frame,
+    waypoints = S7::class_list,
+    plot = S7::new_property(
+      getter = function(self) {
+        log_debug("Creating map plot.")
+        ggplot2::ggplot(
+          data = self@cells,
+          mapping = ggplot2::aes(
+            x = .data[["x"]],
+            y = .data[["y"]],
+            fill = .data[["type"]]
+          )
+        ) +
+          ggplot2::geom_tile()
+      }
+    )
+  ),
+  constructor = function(
+    cells,
+    emitter,
+    target,
+    waypoints
+  ) {
+    log_trace("Casting length and height to integer.")
+    width <- as.integer(max(cells[["x"]]))
+    height <- as.integer(max(cells[["y"]]))
+    waypoints <- c(
+      list(emitter),
+      waypoints,
+      list(target)
+    )
+    logger::log_trace("Creating S7 map object.")
+    S7::new_object(
+      S7::S7_object(),
+      width = width,
+      height = height,
+      cells = cells,
+      waypoints = waypoints
+    )
+  }
+)
---
+++ R/map_01.R
@@ -0,0 +1,26 @@
+map_01 <- map(
+  cells = data.frame(
+    x = rep(1L:10L, 10L),
+    y = rep(1L:10L, each = 10L),
+    type = "open",
+    stringsAsFactors = TRUE
+  ) |>
+    dplyr::mutate(
+      type = dplyr::case_when(
+        y %in% c(3L, 5L, 7L) ~ "road",
+        y == 4L & x == 10L ~ "road",
+        y == 6L & x == 1L ~ "road",
+        TRUE ~ "open"
+      )
+    ),
+  emitter = c(1L, 3L),
+  target = c(10L, 7L),
+  waypoints = list(
+    c(1L, 3L),
+    c(10L, 1L),
+    c(10L, 5L),
+    c(1L, 5L),
+    c(1L, 7L),
+    c(10L, 7L)
+  )
+)
