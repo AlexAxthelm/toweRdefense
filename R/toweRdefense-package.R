f9306abd776182e181df95015b0cc06755c2d9e8
Alex Axthelm <aaxthelm@RMI.org>
2024-08-07 12:34:28 +0200

    Add logger dependecy

---
+++ R/toweRdefense-package.R
@@ -0,0 +1,22 @@
+#' @keywords internal
+"_PACKAGE"
+
+# Supress lintr warnings about functions re-exported from logger
+utils::globalVariables(
+  c(
+    "log_debug",
+    "log_error",
+    "log_info",
+    "log_trace",
+    "log_warn"
+  )
+)
+
+## usethis namespace: start
+#' @importFrom logger log_debug
+#' @importFrom logger log_fatal
+#' @importFrom logger log_info
+#' @importFrom logger log_trace
+#' @importFrom logger log_warn
+## usethis namespace: end
+NULL
